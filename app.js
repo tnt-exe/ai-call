9f04ce94cca8beffa2efc7a380f80d42971c409a
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-11-25 23:35:33 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -8,6 +9,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
