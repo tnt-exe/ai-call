68bf7894b7148b3056ef15a4158003fb9f337aec
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-11-26 18:21:42 +0700

    use combined logger

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
